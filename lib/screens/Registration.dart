901f5dcaaa6969cc50eceb9ff9aad15bc2560d73
Unistal <spurva19@gmail.com>
2024-03-06 10:26:10 +0530

    addUpdateHive

--- lib/screens/Registration.dart
+++ lib/screens/Registration.dart
@@ -152,7 +152,7 @@ class _RegistrationFormState extends State<RegistrationForm> {
                           fontWeight: FontWeight.bold),
                     ),
                     Text(
-                      "Date : 27-02-2024",
+                      "Date : 06-03-2024",
                       style: TextStyle(
                           color: Colors.green.shade800,
                           fontWeight: FontWeight.bold),
