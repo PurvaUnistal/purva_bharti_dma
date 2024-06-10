8c74c7889a8af26f245a09d8873cbeca028ccfbf
Unistal <spurva19@gmail.com>
2024-06-10 11:06:04 +0530

    CommercialSchemeStatusDone

--- lib/screens/Registration.dart
+++ lib/screens/Registration.dart
@@ -152,7 +152,7 @@ class _RegistrationFormState extends State<RegistrationForm> {
                           fontWeight: FontWeight.bold),
                     ),
                     Text(
-                      "Date : 06-06-2024",
+                      "Date : 10-06-2024",
                       style: TextStyle(
                           color: Colors.green.shade800,
                           fontWeight: FontWeight.bold),
