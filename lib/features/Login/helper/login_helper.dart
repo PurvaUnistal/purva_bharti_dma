485dcb4268bab7f25a27501dbabe4d350906f34d
Unistal <spurva19@gmail.com>
2025-03-05 10:22:37 +0530

    AllDone

--- lib/features/Login/helper/login_helper.dart
+++ lib/features/Login/helper/login_helper.dart
@@ -2,52 +2,73 @@ import 'package:flutter/material.dart';
 import 'package:pbg_app/ExportFile/export_file.dart';
 
 class LoginHelper {
-  static String p = r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+  static String p =
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+
   static Future<dynamic> textFieldValidation(
       {required String email,
-        required password,
-        required BuildContext context}) async {
+      required password,
+      required BuildContext context}) async {
     try {
       if (email.isEmpty) {
-        Utils.errorSnackBar(msg: AppString.emailLabel,context: context);
+        Utils.errorSnackBar(msg: AppString.emailLabel, context: context);
         return false;
       } else if (password.isEmpty) {
-        Utils.errorSnackBar(msg: AppString.passwordLabel,context: context);
+        Utils.errorSnackBar(msg: AppString.passwordLabel, context: context);
         return false;
       }
       return true;
     } catch (e) {
       log(e.toString());
-      Utils.errorSnackBar(msg: e.toString(),context: context);
+      Utils.errorSnackBar(msg: e.toString(), context: context);
       return false;
     }
   }
 
-  static Future<dynamic> loginData({
-    required String emailId,
-    required String password,
-    required BuildContext context}) async {
+  static Future<dynamic> loginData(
+      {required String emailId,
+      required String password,
+      required BuildContext context}) async {
     var param = {
-      "email" : emailId,
-      "password" : password,
+      "email": emailId,
+      "password": password,
     };
     try {
-      var res = await ApiServer.postData(urlEndPoint: AppUrl.auth, context: context, body: json.encode(param));
-      if(res != null && res["status"] == 200 && res["messages"] == "User logged In successfully"){
-        return LoginModel.fromJson(res);
-      } if(res != null && res["status"] == 401){
-        if(res["messages"] == "Unauthorised"){
-          return Utils.errorSnackBar(msg : res["messages"],context:context);
+      var res = await ApiServer.postData(
+        urlEndPoint: AppUrl.auth,
+        context: context,
+        body: json.encode(param),
+      );
+      if (res != null && res["status"] == 200) {
+        if (res["user"]["role"] == "dma" && res["messages"] == "User logged In successfully") {
+          return LoginModel.fromJson(res);
         } else {
-          return Utils.errorSnackBar(msg : res["messages"]["email"],context: context);
+          Utils.errorSnackBar(
+              msg: "Invalid role ID. Please check your credentials.",
+              context: context);
+          return null;
         }
-      }else {
-        return Utils.errorSnackBar(msg : res,context: context);
       }
+      if (res != null && res["status"] == 401) {
+        if (res["messages"] == "Unauthorised") {
+          Utils.errorSnackBar(msg: res["messages"], context: context);
+        } else if (res["messages"] is Map &&
+            res["messages"].containsKey("email")) {
+          Utils.errorSnackBar(msg: res["messages"]["email"], context: context);
+        } else {
+          Utils.errorSnackBar(
+              msg: "Unknown authentication error", context: context);
+        }
+        return null;
+      }
+      Utils.errorSnackBar(
+          msg: "Something went wrong. Please try again.", context: context);
+      return null;
     } catch (e) {
       log("catchLogin-->${e.toString()}");
-      // return null;
+      Utils.errorSnackBar(
+          msg: "An error occurred. Please try again.", context: context);
+      return null;
     }
-
   }
 }
--- lib/features/dashboard/presentation/widget/logout_widget.dart
+++ lib/features/dashboard/presentation/widget/logout_widget.dart
@@ -7,44 +7,57 @@ class LogoutWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height/4.9,
-      margin: EdgeInsets.all(MediaQuery.of(context).size.height * 0.04),
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height / 5,
+      margin: EdgeInsets.symmetric(horizontal: 10),
       child: SingleChildScrollView(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(AppString.logout+"?", style: Styles.title,),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Text(
+              AppString.logout + "?",
+              style: Styles.stars,
+            ),
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.01,
             ),
-            Text(AppString.logoutMsg, style: Styles.title,),
+            Text(
+              AppString.logoutMsg,
+              style: Styles.title,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.01,
+            ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Flexible(
                   flex: 2,
-                  child: ButtonWidget(text: AppString.logout,
+                  child: ButtonWidget(
+                      text: AppString.logout,
                       onPressed: () async {
                         Navigator.pushAndRemoveUntil(
                             context,
-                            MaterialPageRoute(builder: (context) => const LoginView()),
-                                (route) => false
-                        );
+                            MaterialPageRoute(
+                                builder: (context) => const LoginView()),
+                            (route) => false);
                         await SharedPref.clearAll();
-                      }
-                  ),
+                      }),
                 ),
                 SizedBox(
                   width: MediaQuery.of(context).size.width * 0.03,
                 ),
-
                 Flexible(
                   flex: 2,
                   child: ButtonWidget(
                       text: AppString.no,
                       onPressed: () {
                         Navigator.of(context).pop();
-                      }
-                  ),
+                      }),
                 ),
               ],
             )
